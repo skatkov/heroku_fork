3e76bbbbe52cc7c41c5de74aaf53106f24f72136
Simon Mathieu <simon.math@gmail.com>
2014-01-17 16:48:48 -0800

    Fix script

--- fork.rb
+++ fork.rb
@@ -1,6 +1,10 @@
+$LOAD_PATH.unshift File.dirname(File.expand_path(__FILE__))
+
 require 'heroku'
 require 'heroku/command/fork'
 require 'heroku/command/run'
+require 'heroku/client/pgbackups'
+
 
 @args = ARGV
 raise "Not enough args" if @args.length < 3
@@ -9,12 +13,10 @@ new_repo_name = @args[1]
 branch_name = @args[2]
 
 Heroku::Command.run("fork", %W"-a #{original_repo_name} #{new_repo_name}")
-# fork = Heroku::Command::Fork.new([new_repo_name], { app: original_repo_name})
-# fork.index()
 
 system "git push -f git@heroku.com:#{new_repo_name}.git #{branch_name}:master"
 
-Heroku::Command.run("run", %W"run #{new_repo_name} ./post_script")
+Heroku::Command.run("run", %W"./post_fork --app #{new_repo_name}")
 
 
 
